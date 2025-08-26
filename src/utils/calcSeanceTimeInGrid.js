67b412827ab61d99acf67fa2c767d536f9342088
Roman <CurajPompei@yandex.ru>
2025-08-26 20:00:14 +0300

    pages init

--- src/utils/calcSeanceTimeInGrid.js
+++
@@ -1,9 +0,0 @@
-const calcSeanceTimeInGrid = (time) => {
-  const [hours, minutes] = time.split(':');
-  const minutesInDay = 60 * 24;
-  const timeInMinutes = +hours * 60 + +minutes;
-  const timeInPercent = timeInMinutes * 100 / minutesInDay;
-  return timeInPercent;
-};
-
-export default calcSeanceTimeInGrid;
